e2242ab2ad2eda1aede30583bb8dbafa9b8a21ce
GJZ26 <adolfogj26@gmail.com>
2024-04-07 15:28:12 -0600

    censuring

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav>
       <h1>Generative Calendar</h1>
-      <a href="https://github.com/GJZ26/Generative-Calendar" target="_blank">
+      <a href="#" target="_blank">
         Source
       </a>
     </nav>
